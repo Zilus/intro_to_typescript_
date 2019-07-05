2f0a50b9a5628de8ab0daeb8a0e76d0337ee9522
Oscar Azpeitia <oscar@ecocode.de>
2019-07-05 16:47:21 +0200

    For of and for in

---
+++ 014_loops.js
@@ -0,0 +1,12 @@
+var players = [3, 10, 4, 5, 1];
+//for in
+for (var player in players) {
+    console.log(player);
+}
+console.log("for of:");
+//for of
+for (var _i = 0, players_1 = players; _i < players_1.length; _i++) {
+    var player = players_1[_i];
+    console.log(player);
+}
+//# sourceMappingURL=014_loops.js.map
---
+++ 014_loops.ts
@@ -0,0 +1,12 @@
+let players: number[] = [3, 10, 4, 5, 1];
+
+//for in
+for (let player in players) {
+    console.log(player);    
+}
+
+console.log("for of:");  
+//for of
+for (let player of players) {
+    console.log(player);    
+}
