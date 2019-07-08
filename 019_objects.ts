bfafe1393ba7937d6b70662c7dbdfad35b47f998
Oscar Azpeitia <oscar@ecocode.de>
2019-07-08 11:17:45 +0200

    objects

---
+++ 019_objects.ts
@@ -0,0 +1,14 @@
+var realUserObject = {
+    email: "test@test.com",
+    firstName: "Oscar",
+    lastName: "Azpeitia",
+
+    sayHi() {
+        return "Hey there";
+    }
+};
+
+console.log(realUserObject.firstName);
+console.log(realUserObject.lastName);
+console.log(realUserObject.email);
+console.log(realUserObject.sayHi());
