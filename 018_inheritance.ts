baaf73f55db47bc4031564e97c27c62b33d14815
Oscar Azpeitia <oscar@ecocode.de>
2019-07-08 11:11:55 +0200

    inheritance

---
+++ 018_inheritance.ts
@@ -0,0 +1,40 @@
+class Report 
+{
+    companyProfile: string;
+
+    constructor(name: string) {
+        this.companyProfile = name;
+    }
+}
+
+class Invoice extends Report
+{
+    constructor(public name: string, public total: number) 
+    { 
+        super(name); 
+    }
+
+    printInvoice() {
+        return this.name + ", " + this.total;
+    }
+}
+
+class BillOfLading extends Report
+{
+    constructor(public name: string, public city: string, public state: string) 
+    { 
+        super(name); 
+    }
+
+    printBol() {
+        return this.name + ", " + this.city + ", " + this.state;
+    }
+}
+
+var invoice = new Invoice('Google', 200);
+var bol = new BillOfLading('Google', 'MV', 'Cal');
+
+console.log(invoice.companyProfile); 
+console.log(invoice.printInvoice()); 
+console.log(bol.printBol()); 
+
